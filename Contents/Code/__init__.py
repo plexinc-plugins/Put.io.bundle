c9dc7537f586230abba2ad87c33de1be41db08fc
Cenk Alti <cenkalti@gmail.com>
2017-04-10 17:58:04 +0300

    pep8

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,5 @@
 import putio2
 
-
 NAME = 'Put.io'
 
 ART = 'art-default.jpg'
@@ -8,26 +7,26 @@ ICON = 'icon-default.png'
 
 
 def Start():
-    Plugin.AddViewGroup('InfoList', viewMode = 'InfoList', mediaType = 'items')
-    Plugin.AddViewGroup('List', viewMode = 'List', mediaType = 'items')
-    Plugin.AddViewGroup('Photos', viewMode = 'List', mediaType = 'photos')
-    
+    Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
+    Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
+    Plugin.AddViewGroup('Photos', viewMode='List', mediaType='photos')
+
     ObjectContainer.art = R(ART)
     ObjectContainer.title1 = NAME
-    
+
     DirectoryItem.thumb = R(ICON)
 
 
-@handler('/video/putio', NAME, art = ART, thumb = ICON)
+@handler('/video/putio', NAME, art=ART, thumb=ICON)
 def MainMenu():
     return ParseDirectory(0, NAME)
 
 
 @route('/video/putio/directory/{id}')
 def ParseDirectory(id, name):
-    oc = ObjectContainer(title1 = name, view_group = 'InfoList')
+    oc = ObjectContainer(title1=name, view_group='InfoList')
 
-    oc.add(PrefsObject(title = L('Preferences')))
+    oc.add(PrefsObject(title=L('Preferences')))
 
     token = Prefs['access_token']
     if token == "":
@@ -36,33 +35,32 @@ def ParseDirectory(id, name):
 
     client = putio2.Client(token)
 
-
     try:
         for f in client.File.list(id):
             if f.content_type == 'application/x-directory':
                 oc.add(DirectoryObject(
-                    key = Callback(ParseDirectory, id = f.id, name = f.name),
-                    title = f.name))
-            
+                    key=Callback(ParseDirectory, id=f.id, name=f.name),
+                    title=f.name))
+
             elif f.content_type.startswith('video/'):
                 oc.add(VideoClipObject(
-                    key = Callback(Lookup, id = f.id),
-                    items = [ MediaObject(parts = [ PartObject(key = Callback(PlayMedia, id = f.id)) ]) ],
-                    rating_key = f.id,
-                    title = f.name,
-                    thumb = f.screenshot))
-            
+                    key=Callback(Lookup, id=f.id),
+                    items=[MediaObject(parts=[PartObject(key=Callback(PlayMedia, id=f.id))])],
+                    rating_key=f.id,
+                    title=f.name,
+                    thumb=f.screenshot))
+
             elif f.content_type.startswith('audio/'):
                 oc.add(TrackObject(
-                    key = Callback(Lookup, id = f.id),
-                    items = [ MediaObject(parts = [ PartObject(key = Callback(PlayMedia, id = f.id)) ]) ],
-                    rating_key = f.id,
-                    title = f.name,
-                    thumb = f.screenshot))
-            
+                    key=Callback(Lookup, id=f.id),
+                    items=[MediaObject(parts=[PartObject(key=Callback(PlayMedia, id=f.id))])],
+                    rating_key=f.id,
+                    title=f.name,
+                    thumb=f.screenshot))
+
             else:
                 Log.Info("Unsupported content type '%s'" % f.content_type)
-    except:
+    except Exception:
         Log.Exception("Files couldn't fetch. Access token is wrong or missing.")
 
     return oc
@@ -72,29 +70,29 @@ def ParseDirectory(id, name):
 def Lookup(id):
     oc = ObjectContainer()
     id = int(id)
-    
+
     client = putio2.Client(Prefs['access_token'])
     f = client.File.get(id)
-    
+
     if f.content_type.startswith('video/'):
         oc.add(VideoClipObject(
-            key = Callback(Lookup, id = f.id),
-            items = [ MediaObject(parts = [ PartObject(key = Callback(PlayMedia, id = f.id)) ]) ],
-            rating_key = f.id,
-            title = f.name,
-            thumb = f.screenshot))
-    
+            key=Callback(Lookup, id=f.id),
+            items=[MediaObject(parts=[PartObject(key=Callback(PlayMedia, id=f.id))])],
+            rating_key=f.id,
+            title=f.name,
+            thumb=f.screenshot))
+
     elif f.content_type.startswith('audio/'):
         oc.add(TrackObject(
-            key = Callback(Lookup, id = f.id),
-            items = [ MediaObject(parts = [ PartObject(key = Callback(PlayMedia, id = f.id)) ]) ],
-            rating_key = f.id,
-            title = f.name,
-            thumb = f.screenshot))
-    
+            key=Callback(Lookup, id=f.id),
+            items=[MediaObject(parts=[PartObject(key=Callback(PlayMedia, id=f.id))])],
+            rating_key=f.id,
+            title=f.name,
+            thumb=f.screenshot))
+
     else:
         Log.Info("Unsupported content type '%s'" % f.content_type)
-    
+
     return oc
 
 
